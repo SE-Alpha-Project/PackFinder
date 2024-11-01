e8f4b54d5afdc2d81f6c9b9364b98eaa4e44fa81
yash2705 <62518145+yash2705@users.noreply.github.com>
2024-11-01 18:31:46 -0400

    Changed the forgot password pages UI (#6)
    * fixed flake8 linting errors
    
    * removed white spacing and added extralines to hopefully fix all flake8 errors now
    
    * worked on all the flake8 warnings
    
    * removed whitespace
    
    * worked on the forgot password pages ui
    
    ---------
    
    Co-authored-by: Yash Shah <yashshah@Mac.lan>
    Co-authored-by: Yash Shah <yashshah@Yashs-Air.lan>

--- src/tests/test_navbar.py
+++ src/tests/test_navbar.py
@@ -1,20 +1,14 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
+
 
 class NavbarTests(TestCase):
     def test_navbar_unauthenticated_links(self):
         """Test that unauthenticated users see correct nav links"""
         response = self.client.get(reverse('home'))
-        
-        # Check status code
         self.assertEqual(response.status_code, 200)
-        
-        # Check for public links
         self.assertContains(response, 'href="/about"')
         self.assertContains(response, 'href="/signup"')
-        
-        # Check authenticated-only links are not present
         self.assertNotContains(response, 'href="/profile"')
         self.assertNotContains(response, 'href="/myroom"')
         self.assertNotContains(response, 'href="/findpeople"')
@@ -23,17 +17,13 @@ class NavbarTests(TestCase):
     def test_navbar_brand_link(self):
         """Test that the PackFinder brand link is present"""
         response = self.client.get(reverse('home'))
-        
         self.assertContains(response, '🐺 PackFinder')
         self.assertContains(response, 'href="/"')
 
     def test_about_link_always_visible(self):
         """Test that the About link is always visible regardless of auth status"""
-        # Test without authentication
         response = self.client.get(reverse('home'))
         self.assertContains(response, 'href="/about"')
-        
-        # Test with authentication
         self.client.login(username='testuser', password='testpass123')
         response = self.client.get(reverse('home'))
-        self.assertContains(response, 'href="/about"') 
+        self.assertContains(response, 'href="/about"')
